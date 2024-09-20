a7732d26878b7358b7e63967dc33aab50be4964a
andres-na-dev-admin <manazutec@gmail.com>
2024-09-19 19:35:39 -0600

    AndresNa: inital signals

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { signal } from '@angular/core';
+import { Component} from '@angular/core';
+import {CommonModule} from '@angular/common'
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import {Task} from './../../shared/models/task';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './home.component.html'
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  tasks = signal<Task[]>([]);
+
+  filter = signal<'all' | 'pending' | 'completed'>('all');
 
 }
